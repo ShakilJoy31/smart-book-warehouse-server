1ee4b3ce1ea27b51babc95a8a106c6a6b3c527b7
ShakilJoy31 <joy31a@gmail.com>
2022-04-28 23:18:15 +0600

    Connecting to the database is done

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const cors = require('cors');
 app.use(cors());  
 require('dotenv').config();
 app.use(express.json()); 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASS}@cluster0.bkjf1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 
 app.get('/', (req, res)=>{
