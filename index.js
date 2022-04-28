ccfbab2252012fad085f155f315f88692901a503
ShakilJoy31 <joy31a@gmail.com>
2022-04-29 01:32:46 +0600

    Getting data from database is done

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASS}@cluster0.bkjf1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        await client.connect(); 
+        const booksCollection = client.db('smartBook').collection('allBook'); 
+
+        app.get('/books', async (req, res)=>{
+            const query = {}; 
+            const cursor = booksCollection.find(query); 
+            const books = await cursor.toArray(); 
+            res.send(books); 
+        }); 
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir); 
+
 
 
 
